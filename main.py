1422ab3beb4aa17e991f6a4823a35e5b7c79da9a
Phil Kendall <philiprobertovich@gmail.com>
2022-11-14 12:08:49 -0800

    format galaxy function.

--- main.py
+++ main.py
@@ -30,23 +30,30 @@ my_dads_shoe.euro_size()
 def galaxy(name, *space_debris, pluto_is_planet=True,**planet_colors):
   print(f'''
     The {name} is a galaxy in our universe. \n
-    This galaxy contains vasts amounts of different space matter including \n
-    {space_debris[0, -2]}, and {space_debris[-1]}. \n
+    This galaxy contains vasts amounts of different 
+    space matter including {space_debris}. \n
     In this galaxy, there are {len(planet_colors)} planets. \n
     Based on recent astronomical studies, astronomers have \n
-    decided that the statement of Pluto being a planent is {pluto_is_planet}. 
+    decided that the statement of Pluto being a planent is {pluto_is_planet}.  \n
+    Here is a list of the planets and their colors:
   ''')
-  print('Here is a list of the planets and their colors:')
-  for key, value in planet_colors.items():
-    print(f'{planet_colors[key]} is {planet_colors[value]}.')
+  for key in planet_colors.keys():
+    print(f'''
+    {key.capitalize()} is {planet_colors[key]}.
+    ''')
 
-
-mercury='gray',
-venus='yellowish white',
-earth='blue and green',
-mars='red',
-saturn='yellow-brown',
-jupiter='orange',
-uranus='light blue',
-neptune='deep see blue',
-pluto='brown'
+galaxy(
+  "Milky Way Galaxy", 
+  'astroids', 
+  'space dust', 
+  'leprachauns', 
+  mercury='gray',
+  venus='yellowish white',
+  earth='blue and green',
+  mars='red',
+  saturn='yellow-brown',
+  jupiter='orange',
+  uranus='light blue',
+  neptune='deep see blue',
+  pluto='brown'
+)
