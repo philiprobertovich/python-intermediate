1e57391b50b5c7fcc7b1f5844d6ae405a89a50ff
Phil Kendall <philiprobertovich@gmail.com>
2022-11-14 10:26:55 -0800

    create working lambda subtracting function

--- main.py
+++ main.py
@@ -6,4 +6,7 @@ def fourth_place(*args):
       return args[3]
     except IndexError as error:
       print(error)
-      
+      print("The list is shorter than 4 items")
+
+subtracter_lambda = lambda num_1, num_2: num_1 - num_2
+(subtracter_lambda)(9, 5)
