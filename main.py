0239446e54b885390dd7cfa0e5e6a807fe52db75
Phil Kendall <philiprobertovich@gmail.com>
2022-11-14 10:44:20 -0800

    create Shoe class with euro_size function and two instaces of it.

--- main.py
+++ main.py
@@ -10,3 +10,20 @@ def fourth_place(*args):
 
 subtracter_lambda = lambda num_1, num_2: num_1 - num_2
 (subtracter_lambda)(9, 5)
+
+class Shoe:
+  def __init__(self, US_size, color, smelly=False):
+    self.US_size = US_size
+    self.color = color
+    self.smelly = smelly
+
+  def euro_size(self):
+    convert = self.US_size + 33
+    print(f'Your size in European measurements is {convert} cm.')
+
+my_shoe = Shoe(10, 'Black', smelly=False)
+my_shoe.euro_size()
+
+my_dads_shoe = Shoe(9, 'Blue', smelly=True)
+my_dads_shoe.euro_size()
+  
